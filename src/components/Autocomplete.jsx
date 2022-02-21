b1a392d46d4e4542c340037e2924262a85283964
GriffBlack <GriffBlack42018@gmail.com>
2022-02-21 03:31:18 +0300

    targ

--- src/components/Autocomplete.jsx
+++ src/components/Autocomplete.jsx
@@ -1,4 +1,4 @@
-import { Component, useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import data from '../data.json';
 
 
@@ -7,26 +7,42 @@ const Autocomplete = () => {
      const [value, setValue] = useState('');
 
      const filteredAnimals = data.filter((data) => {
-       return data.type.toLowerCase().includes(value.toLowerCase());
+       return data.name.toLowerCase().includes(value.toLowerCase());
      });
 
-    //  const takeType = (data) => {
-    //    const typeAnimal = [];
+  const takeType = (data) => {
 
-    //    return typeAnimal;
-    //  };
+    const typeAnimal = [];
+    data.forEach((data) => {
+        if (!typeAnimal.includes(data.type))
+          typeAnimal.push(data.type);
+
+      return typeAnimal;
+    })
+  };
+
+  
+  useEffect(() => {
+    takeType(data)
+    
+  });
      
    return (
      <div className='form'>
        <input
          type='text'
-         placeholder='Enter the animal'
+         placeholder='Enter name'
          className='inputAuto'
          onChange={(event) => setValue(event.target.value)}
        />
+       {/* <select>
+         {typeAnimal.map((data, index) => {
+           return <option>{typeAnimal.index}</option>;
+         })}
+       </select> */}
        <select>
          {filteredAnimals.map((data, index) => {
-           return <option>{data.name}</option>;
+           return <option key={data.id}>{data.name}</option>;
          })}
        </select>
      </div>
